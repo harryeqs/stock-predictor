1859a026e2cb283cc5e8ca20ce67dca935728208
Harry-QY <qy250@cam.ac.uk>
2024-10-25 15:50:58 +0100

    brainstorm

---
+++ main.py
@@ -0,0 +1,8 @@
+# Create a multi-agent system to predict stock price
+# Input: Stock name + price
+# Create three prices (by code)
+# Let agents to guess which is the real price
+# Three functions:
+## Search function
+## Firecrawl function
+## Answer submission function
